e0ed11c8d07d0f29739bcd555709432bb715a2a4
Young <Young980421@outlook.com>
2020-06-21 22:42:04 +0800

    增加了排序、希尔算法

--- src/TestSort.java
+++ src/TestSort.java
@@ -5,13 +5,16 @@
 public class TestSort {
 
     public static void main(String[] args) {
-        int ar[] = {5,6,72,3,1,56,};
-        int arr[] = insert(ar);
-        for (int i = 0; i < arr.length; i++){
-            System.out.println(arr[i]);
+        int ar[] = {5, 6, 72, 3, 1, 56,};
+        Solution.Rapid(ar, 0, ar.length - 1);
+        for (int i = 0; i < ar.length; i++) {
+            System.out.println(ar[i]);
         }
 
     }
+
+
+
 
 
 }
