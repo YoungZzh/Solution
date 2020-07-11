c0a37a4c467043de7ff306b6d6c4e27987003a34
Young <Young980421@outlook.com>
2020-07-11 22:20:32 +0800

    增加了leetcode的第8题

--- src/TestSort.java
+++ src/TestSort.java
@@ -6,21 +6,8 @@ public class TestSort {
 
     public static void main(String[] args) {
 
-        System.out.println(reverse(-2147483648));
+        System.out.println();
     }
-    public static int reverse(int x) {
-        long temp = 0;
 
-        while(x != 0){
-            int pop = x % 10;
-            temp = temp * 10 + pop;
-
-            if(temp > Integer.MAX_VALUE || temp < Integer.MIN_VALUE){
-                return 0;
-            }
-            x /= 10;
-        }
-        return (int)temp;
-    }
 
 }
