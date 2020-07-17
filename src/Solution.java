b86d9215d500ff0614ea207f38f4e1d8171feaef
Young <Young980421@outlook.com>
2020-07-17 20:54:45 +0800

    增加了leetcode的第10题(采用递归，动态规划方法未理解)

--- src/Solution.java
+++ src/Solution.java
@@ -685,35 +685,36 @@ public class Solution {
     }
 
 
-
     /**
      * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
-     *
+     * <p>
      * 示例 1：
-     *
+     * <p>
      * 输入: "babad"
      * 输出: "bab"
      * 注意: "aba" 也是一个有效答案。
+     *
      * @param s
      * @return
      */
     public String longestPalindrome(String s) {
-        if(s.length() < 2){
+        if (s.length() < 2) {
             return s;
         }
-        int max=1;
+        int max = 1;
         int index = 0;
         char[] str = s.toCharArray();
-        for(int i=0;i<=str.length-1;i++){
-            for(int j=i+1;j<str.length;j++){
-                if(j-i+1>max && validPalindromic(str,i,j)){
+        for (int i = 0; i <= str.length - 1; i++) {
+            for (int j = i + 1; j < str.length; j++) {
+                if (j - i + 1 > max && validPalindromic(str, i, j)) {
                     index = i;
-                    max = j-i+1;
+                    max = j - i + 1;
                 }
             }
         }
-        return s.substring(index,max+index);
+        return s.substring(index, max + index);
     }
+
     private boolean validPalindromic(char[] charArray, int left, int right) {
         while (left < right) {
             if (charArray[left] != charArray[right]) {
@@ -728,33 +729,32 @@ public class Solution {
 
     /**
      * 将一个给定字符串根据给定的行数，以从上往下、从左到右进行 Z 字形排列。
-     *
+     * <p>
      * 比如输入字符串为 "LEETCODEISHIRING" 行数为 3 时，排列如下：
-     *
+     * <p>
      * L   C   I   R
      * E T O E S I I G
      * E   D   H   N
-     *
      */
     public String convert(String s, int numRows) {
 
-        if(numRows<2){
+        if (numRows < 2) {
             return s;
         }
         List<StringBuilder> list = new ArrayList<>();
         int i = 0;
         int flag = -1;
-        for(int n=0;n<numRows;n++){
+        for (int n = 0; n < numRows; n++) {
             list.add(new StringBuilder());
         }
-        for(char c : s.toCharArray()){
+        for (char c : s.toCharArray()) {
             list.get(i).append(c);
-            if(i==0||i==numRows-1)
+            if (i == 0 || i == numRows - 1)
                 flag = -flag;
             i += flag;
         }
         StringBuilder res = new StringBuilder();
-        for(StringBuilder row : list){
+        for (StringBuilder row : list) {
             res.append(row);
         }
         return res.toString();
@@ -762,81 +762,80 @@ public class Solution {
 
     /**
      * 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
-     *
+     * <p>
      * 我们可以一次构建反转整数的一位数字。在这样做的时候，我们可以预先检查向原整数
      * 附加另一位数字是否会导致溢出。
      * 示例 1:
-     *
+     * <p>
      * 输入: 123
      * 输出: 321
      *  示例 2:
-     *
+     * <p>
      * 输入: -123
      * 输出: -321
+     *
      * @param x
      * @return
      */
     public int reverse(int x) {
         long temp = 0;
-        while (x != 0){
+        while (x != 0) {
             int pop = x % 10;
-            temp = temp*10 + pop;
-            if (temp > Integer.MAX_VALUE || temp < Integer.MIN_VALUE){
+            temp = temp * 10 + pop;
+            if (temp > Integer.MAX_VALUE || temp < Integer.MIN_VALUE) {
                 return 0;
             }
             x /= 10;
         }
-        return (int)temp;
+        return (int) temp;
     }
 
 
     /**
      * 请你来实现一个 atoi 函数，使其能将字符串转换成整数。
-     *
+     * <p>
      * 首先，该函数会根据需要丢弃无用的开头空格字符，直到寻找到第一个非空格的字符为止。接下来的转化规则如下：
-     *
+     * <p>
      * 如果第一个非空字符为正或者负号时，则将该符号与之后面尽可能多的连续数字字符组合起来，形成一个有符号整数。
      * 假如第一个非空字符是数字，则直接将其与之后连续的数字字符组合起来，形成一个整数。
      * 该字符串在有效的整数部分之后也可能会存在多余的字符，那么这些字符可以被忽略，它们对函数不应该造成影响。
      * 注意：假如该字符串中的第一个非空格字符不是一个有效整数字符、字符串为空或字符串仅包含空白字符时，则你的函数不需要进行转换，即无法进行有效转换。
-     *
+     * <p>
      * 在任何情况下，若函数不能进行有效的转换时，请返回 0 。
-     *
+     * <p>
      * 提示：
-     *
+     * <p>
      * 本题中的空白字符只包括空格字符 ' ' 。
      * 假设我们的环境只能存储 32 位大小的有符号整数，那么其数值范围为 [−231,  231 − 1]。如果数值超过这个范围，请返回  INT_MAX (231 − 1) 或 INT_MIN (−231) 。
-     *
      */
 
     public static int myAtoi(String str) {
-        if(str.trim().equals("")){
+        if (str.trim().equals("")) {
             return 0;
         }
         char[] s = str.trim().toCharArray();
         StringBuilder sb = new StringBuilder();
-        if (s[0] == '-' || s[0] == '+' || (s[0] >= 48 && s[0] < 58) ){
+        if (s[0] == '-' || s[0] == '+' || (s[0] >= 48 && s[0] < 58)) {
             sb.append(s[0]);
-        }else {
+        } else {
             return 0;
         }
-        for (int i = 1; i < s.length; i++){
-            if (s[i] >= 48 && s[i] < 58){
+        for (int i = 1; i < s.length; i++) {
+            if (s[i] >= 48 && s[i] < 58) {
                 sb.append(s[i]);
-                if ( Long.valueOf(sb.toString()) <= Integer.MIN_VALUE){
+                if (Long.valueOf(sb.toString()) <= Integer.MIN_VALUE) {
                     return Integer.MIN_VALUE;
                 }
-                if(Long.valueOf(sb.toString()) >= Integer.MAX_VALUE ){
+                if (Long.valueOf(sb.toString()) >= Integer.MAX_VALUE) {
                     return Integer.MAX_VALUE;
                 }
-            }else {
+            } else {
                 break;
             }
         }
 
-        return (!sb.toString().equals("-") && !sb.toString().equals("+")) ?Integer.valueOf(sb.toString()):0;
+        return (!sb.toString().equals("-") && !sb.toString().equals("+")) ? Integer.valueOf(sb.toString()) : 0;
     }
-
 
 
     public boolean isPalindrome(int x) {
@@ -860,6 +859,44 @@ public class Solution {
         // 由于处于中位的数字不影响回文（它总是与自己相等），所以我们可以简单地将其去除。
         return x == revertedNumber || x == revertedNumber / 10;
     }
+
+
+    /**
+     * 给你一个字符串 s 和一个字符规律 p，请你来实现一个支持 '.' 和 '*' 的正则表达式匹配。
+     * <p>
+     * '.' 匹配任意单个字符
+     * '*' 匹配零个或多个前面的那一个元素
+     * 所谓匹配，是要涵盖 整个 字符串 s的，而不是部分字符串。
+     * <p>
+     * 说明:
+     * <p>
+     * s 可能为空，且只包含从 a-z 的小写字母。
+     * p 可能为空，且只包含从 a-z 的小写字母，以及字符 . 和 *。
+     * 示例 1:
+     * <p>
+     * 输入:
+     * s = "aa"
+     * p = "a"
+     * 输出: false
+     * 解释: "a" 无法匹配 "aa" 整个字符串。
+     */
+
+    public boolean isMatch(String s, String p) {
+        //如果正则串p为空字符串s也为空这匹配成功，如果正则串p为空但是s不是空则说明匹配失败
+        if (p.isEmpty())return s.isEmpty();
+        //判断s和p的首字符是否匹配，注意要先判断s不为空
+        boolean headMatched=!s.isEmpty()&&(s.charAt(0)==p.charAt(0)||p.charAt(0)=='.');
+        if (p.length()>=2&&p.charAt(1)=='*'){//如果p的第一个元素的下一个元素是*
+            //则分别对两种情况进行判断
+            return isMatch(s,p.substring(2))||
+                    (headMatched&&isMatch(s.substring(1),p));
+        }else if (headMatched){//否则，如果s和p的首字符相等
+            return isMatch(s.substring(1),p.substring(1));
+        }else {
+            return false;
+        }
+    }
+
 
 }
 
